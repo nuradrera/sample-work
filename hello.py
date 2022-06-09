c82de2e189871eef4cfeb9378af77042bb10f76f
nuradrera <107099106+nuradrera@users.noreply.github.com>
2022-06-09 12:47:31 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,12 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 
 st.header("My first Streamlit App")
-st.title("This is a title")
 
-st.write("""
-# This is a first-level heading
-## This is a second-level heading
-""")
-
-st.write(pd.DataFrame({
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
     'Intplan': ['yes', 'yes', 'yes', 'no'],
     'Churn Status': [0, 0, 0, 1]
 }))
